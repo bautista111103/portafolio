17f7ea96b871f742c65e77c6a4b0c732de24c028
my_github_bautista leguizamo <leguizamobautista@gmail.com>
2022-09-12 16:27:33 -1200

    retoques

--- src/componentes/Proyectos.js
+++ src/componentes/Proyectos.js
@@ -10,7 +10,7 @@ function ContenedorProyectos() {
     return (
         <div className="Contenedor-de-proyectos">
             <Card className="carta-proyectos">
-            <Card.Body className="cuerpo-carta-proyecto">
+                <Card.Body className="cuerpo-carta-proyecto">
                     <Card.Title>Proyecto 1</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">AmazingEvents</Card.Subtitle>
                     <Card.Img className="imagen-proyectos" src={amazing} />
@@ -23,7 +23,7 @@ function ContenedorProyectos() {
                             <li>JavaScript</li>
                             <li>Css</li>
                             <li>HTML5</li>
-                        </ol>      
+                        </ol>
                     </Card.Text>
                     <div className="links">
                         <Card.Link className="link-carta-proyecto" href="https://amazingevents-leguizamo.netlify.app/">Pagina</Card.Link>
@@ -32,12 +32,20 @@ function ContenedorProyectos() {
                 </Card.Body>
             </Card>
             <Card className="carta-proyectos">
-            <Card.Body className="cuerpo-carta-proyecto">
+                <Card.Body className="cuerpo-carta-proyecto">
                     <Card.Title>Proyecto 2</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">Mindy</Card.Subtitle>
                     <Card.Img className="imagen-proyectos" src={mindy} />
                     <Card.Text className="descripcion-proyectos">
-                        Mindy es un proyecto que me toco realizar de manera grupal, fue dedicada a vender articulos para cualquier tipo de mascotas y nos llevo aproximadamnete una semana realizarla por completo.
+                        Mindy es un proyecto que me toco realizar de manera grupal, es un ecommerce de articulos para cualquier tipo de mascotas y nos llevo aproximadamnete una semana realizarla por completo.
+                    </Card.Text>
+                    <Card.Text className="texto-proyectos-tecnologias">
+                        Tecnologias usadas:
+                        <ol>
+                            <li>JavaScript</li>
+                            <li>Css</li>
+                            <li>HTML5</li>
+                        </ol>
                     </Card.Text>
                     <div className="links">
                         <Card.Link className="link-carta-proyecto" href="https://62fbe15f389d0b371555441f--mindy-petshop-leguizamo.netlify.app/pages/shoptoys">Pagina</Card.Link>
@@ -46,13 +54,23 @@ function ContenedorProyectos() {
                 </Card.Body>
             </Card>
             <Card className="carta-proyectos">
-            <Card.Body className="cuerpo-carta-proyecto">
+                <Card.Body className="cuerpo-carta-proyecto">
                     <Card.Title>Proyecto 3</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">Mytinerary</Card.Subtitle>
                     <Card.Img className="imagen-proyectos" src={mytinerary} />
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                    <Card.Text className="descripcion-proyectos">
+                        Este es un proyecto realizado por mi para una agencia de viaje, me llevo aproximadamnete 2 semanas ternminarla por completo
+                    </Card.Text>
+                    <Card.Text className="texto-proyectos-tecnologias">
+                        Tecnologias usadas:
+                        <ol>
+                            <li>JavaScript</li>
+                            <li>Css</li>
+                            <li>HTML5</li>
+                            <li>React</li>
+                            <li>Redux</li>
+                            <li>Node js</li>
+                        </ol>
                     </Card.Text>
                     <div className="links">
                         <Card.Link className="link-carta-proyecto" href="https://mytinerari-leguizamo-frontend.herokuapp.com/">Pagina</Card.Link>
@@ -65,42 +83,29 @@ function ContenedorProyectos() {
                     <Card.Title>Proyecto 4</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">Gea Store</Card.Subtitle>
                     <Card.Img className="imagen-proyectos" src={gea} />
-                    <Card.Text className="tecnologias-proyectos">
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                    <Card.Text className="descripcion-proyectos">
+                        Este ecommerce tambien fue realizado de manera grupal y tenia como limite de tiempo maximo 10 dias para su entrega en completo funcionamiento.
+                    </Card.Text>
+                    <Card.Text className="texto-proyectos-tecnologias">
+                        Tecnologias usadas:
+                            <ol>
+                                <li>JavaScript</li>
+                                <li>Css</li>
+                                <li>HTML5</li>
+                                <li>React</li>
+                                <li>Redux</li>
+                                <li>Node js</li>
+                                <li>Testing</li>
+                                <li>Upload file</li>
+                            </ol>
                     </Card.Text>
                     <div className="links">
                         <Card.Link className="link-carta-proyecto" href="https://gea-store.herokuapp.com/">Pagina</Card.Link>
                         <Card.Link className="link-carta-proyecto" href="#">Repositorio</Card.Link>
-                    </div>                                                
+                    </div>
                 </Card.Body>
             </Card>
         </div>
     );
 }
-
 export default ContenedorProyectos;
-
-// function ContenedorProyectos() {
-//   return (
-//     <Row xs={1} md={2} className="g-4">
-//       {Array.from({ length: 4 }).map((_, idx) => (
-//         <Col>
-//           <Card>
-//             <Card.Img variant="top" src={foto} />
-//             <Card.Body>
-//               <Card.Title>Card title</Card.Title>
-//               <Card.Text>
-//                 This is a longer card with supporting text below as a natural
-//                 lead-in to additional content. This content is a little bit
-//                 longer.
-//               </Card.Text>
-//             </Card.Body>
-//           </Card>
-//         </Col>
-//       ))}
-//     </Row>
-//   );
-// }
-
-// export default ContenedorProyectos;
